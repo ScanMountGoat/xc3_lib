b6b9dd144f97d049ba81fea5ed1a566341957ef5
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-06-10 22:31:29 -0500

    adjust sampler tests

--- xc3_wgpu/src/sampler.rs
+++ xc3_wgpu/src/sampler.rs
@@ -5,6 +5,7 @@ pub fn create_sampler(device: &wgpu::Device, flags: SamplerFlags) -> wgpu::Sampl
 }
 
 fn sampler_descriptor(flags: SamplerFlags) -> wgpu::SamplerDescriptor<'static> {
+    // TODO: anisotropic filtering and lod bias?
     wgpu::SamplerDescriptor {
         label: None,
         address_mode_u: address_mode(flags.repeat_u(), flags.mirror_u()),
@@ -12,9 +13,13 @@ fn sampler_descriptor(flags: SamplerFlags) -> wgpu::SamplerDescriptor<'static> {
         address_mode_w: wgpu::AddressMode::ClampToEdge,
         mag_filter: filter_mode(flags.nearest()),
         min_filter: filter_mode(flags.nearest()),
-        // TODO: How to disable mipmapping?
-        // TODO: Is this always nearest in game?
         mipmap_filter: wgpu::FilterMode::Nearest,
+        lod_min_clamp: 0.0,
+        lod_max_clamp: if flags.disable_mipmap_filter() {
+            0.25
+        } else {
+            15.0
+        },
         ..Default::default()
     }
 }
@@ -41,7 +46,7 @@ fn address_mode(repeat: bool, mirror: bool) -> wgpu::AddressMode {
 mod tests {
     use super::*;
 
-    // Test various flags values based on testing OpenGL samplers in RenderDoc.
+    // Test various flags values based on testing Vulkan samplers in RenderDoc.
     #[test]
     fn descriptor_0x0() {
         assert_eq!(
@@ -53,6 +58,8 @@ mod tests {
                 mag_filter: wgpu::FilterMode::Linear,
                 min_filter: wgpu::FilterMode::Linear,
                 mipmap_filter: wgpu::FilterMode::Nearest,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 15.0,
                 ..Default::default()
             },
             sampler_descriptor(SamplerFlags::from(0x0))
@@ -70,6 +77,8 @@ mod tests {
                 mag_filter: wgpu::FilterMode::Linear,
                 min_filter: wgpu::FilterMode::Linear,
                 mipmap_filter: wgpu::FilterMode::Nearest,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 15.0,
                 ..Default::default()
             },
             sampler_descriptor(SamplerFlags::from(0b_11))
@@ -87,6 +96,8 @@ mod tests {
                 mag_filter: wgpu::FilterMode::Linear,
                 min_filter: wgpu::FilterMode::Linear,
                 mipmap_filter: wgpu::FilterMode::Nearest,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 15.0,
                 ..Default::default()
             },
             sampler_descriptor(SamplerFlags::from(0b_110))
@@ -104,6 +115,8 @@ mod tests {
                 mag_filter: wgpu::FilterMode::Linear,
                 min_filter: wgpu::FilterMode::Linear,
                 mipmap_filter: wgpu::FilterMode::Nearest,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 15.0,
                 ..Default::default()
             },
             sampler_descriptor(SamplerFlags::from(0b_1100))
@@ -112,7 +125,6 @@ mod tests {
 
     #[test]
     fn descriptor_0x40() {
-        // TODO: this should disable mipmapping
         assert_eq!(
             wgpu::SamplerDescriptor {
                 label: None,
@@ -122,6 +134,8 @@ mod tests {
                 mag_filter: wgpu::FilterMode::Linear,
                 min_filter: wgpu::FilterMode::Linear,
                 mipmap_filter: wgpu::FilterMode::Nearest,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 0.25,
                 ..Default::default()
             },
             sampler_descriptor(SamplerFlags::from(0b_01000000))
@@ -130,7 +144,6 @@ mod tests {
 
     #[test]
     fn descriptor_0x50() {
-        // TODO: this should disable mipmapping
         assert_eq!(
             wgpu::SamplerDescriptor {
                 label: None,
@@ -140,6 +153,8 @@ mod tests {
                 mag_filter: wgpu::FilterMode::Nearest,
                 min_filter: wgpu::FilterMode::Nearest,
                 mipmap_filter: wgpu::FilterMode::Nearest,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 0.25,
                 ..Default::default()
             },
             sampler_descriptor(SamplerFlags::from(0b_01010000))
