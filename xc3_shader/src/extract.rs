260f236d9313e437b14ac27d6e252034e25f66e9
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2023-07-22 16:20:21 -0500

    adjust decompilation process spawning

--- xc3_shader/src/extract.rs
+++ xc3_shader/src/extract.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 
 use crate::annotation::{annotate_fragment, annotate_vertex};
-use xc3_lib::spch::{Slct, Spch};
+use rayon::prelude::*;
+use xc3_lib::spch::{NvsdMetadata, Slct, Spch};
 
 pub fn extract_shader_binaries<P: AsRef<Path>>(
     spch: &Spch,
@@ -9,63 +10,80 @@ pub fn extract_shader_binaries<P: AsRef<Path>>(
     ryujinx_shader_tools: Option<&str>,
     save_binaries: bool,
 ) {
-    for (program_index, program) in spch.shader_programs.iter().enumerate() {
-        // Not all programs have associated names.
-        // Generate the name to avoid any ambiguity.
-        let name = format!("nvsd{program_index}");
+    let output_folder = output_folder.as_ref();
 
-        let slct = program.read_slct(&spch.slct_section);
-        let binaries = vertex_fragment_binaries(spch, &slct);
+    spch.shader_programs
+        .iter()
+        .enumerate()
+        .for_each(|(program_index, program)| {
+            // Not all programs have associated names.
+            // Generate the name to avoid any ambiguity.
+            let name = format!("nvsd{program_index}");
 
-        for (i, (vertex, fragment)) in binaries.into_iter().enumerate() {
-            let vert_file = output_folder.as_ref().join(&format!("{name}_VS{i}.bin"));
-            std::fs::write(&vert_file, vertex).unwrap();
+            let slct = program.read_slct(&spch.slct_section);
+            let binaries = vertex_fragment_binaries(spch, &slct);
 
-            let frag_file = output_folder.as_ref().join(&format!("{name}_FS{i}.bin"));
-            std::fs::write(&frag_file, fragment).unwrap();
+            for (i, (vertex, fragment)) in binaries.into_iter().enumerate() {
+                let vert_file = output_folder.join(&format!("{name}_VS{i}.bin"));
+                std::fs::write(&vert_file, vertex).unwrap();
 
-            // Each NVSD has separate metadata since the shaders are different.
-            let metadata = &slct.nvsds[i].inner;
+                let frag_file = output_folder.join(&format!("{name}_FS{i}.bin"));
+                std::fs::write(&frag_file, fragment).unwrap();
 
-            // This doesn't need to be parsed, so just use debug output for now.
-            let txt_file = output_folder.as_ref().join(&format!("{name}.txt"));
-            let text = format!("{:#?}", &metadata);
-            std::fs::write(txt_file, text).unwrap();
+                // Each NVSD has separate metadata since the shaders are different.
+                let metadata = &slct.nvsds[i].inner;
 
-            // Decompile using Ryujinx.ShaderTools.exe.
-            // There isn't Rust code for this, so just take an exe path.
-            if let Some(shader_tools) = &ryujinx_shader_tools {
-                std::process::Command::new(shader_tools)
-                    .args([&vert_file, &vert_file.with_extension("glsl")])
-                    .output()
-                    .unwrap();
+                // This doesn't need to be parsed, so just use debug output for now.
+                let txt_file = output_folder.join(&format!("{name}.txt"));
+                let text = format!("{:#?}", &metadata);
+                std::fs::write(txt_file, text).unwrap();
 
-                std::process::Command::new(shader_tools)
-                    .args([&frag_file, &frag_file.with_extension("glsl")])
-                    .output()
-                    .unwrap();
+                // Decompile using Ryujinx.ShaderTools.exe.
+                // There isn't Rust code for this, so just take an exe path.
+                if let Some(shader_tools) = &ryujinx_shader_tools {
+                    decompile_glsl_shaders(shader_tools, &frag_file, &vert_file, metadata);
+                }
 
-                // Perform annotation here since we need to know the file names.
-                let mut vert_glsl =
-                    std::fs::read_to_string(vert_file.with_extension("glsl")).unwrap();
-                let mut frag_glsl =
-                    std::fs::read_to_string(frag_file.with_extension("glsl")).unwrap();
-
-                vert_glsl = annotate_vertex(vert_glsl, metadata);
-                std::fs::write(vert_file.with_extension("glsl"), vert_glsl).unwrap();
-
-                frag_glsl = annotate_fragment(frag_glsl, metadata);
-                std::fs::write(frag_file.with_extension("glsl"), frag_glsl).unwrap();
+                // We needed to temporarily create binaries for ShaderTools to decompile.
+                // Delete them if they are no longer needed.
+                if !save_binaries {
+                    std::fs::remove_file(vert_file).unwrap();
+                    std::fs::remove_file(frag_file).unwrap();
+                }
             }
+        });
+}
 
-            // We need to temporarily create binaries for ShaderTools to decompile.
-            // Delete them if they are no longer needed.
-            if !save_binaries {
-                std::fs::remove_file(vert_file).unwrap();
-                std::fs::remove_file(frag_file).unwrap();
-            }
-        }
-    }
+fn decompile_glsl_shaders(
+    shader_tools: &str,
+    frag_file: &Path,
+    vert_file: &Path,
+    metadata: &NvsdMetadata,
+) {
+    // Spawn two processes to decompile both at once.
+    let mut extract_frag = extract_shader(shader_tools, frag_file);
+    let mut extract_vert = extract_shader(shader_tools, vert_file);
+
+    // Wait for decompilation to finish.
+    extract_frag.wait().unwrap();
+    extract_vert.wait().unwrap();
+
+    // Perform annotation here since we need to know the file names.
+    let mut vert_glsl = std::fs::read_to_string(vert_file.with_extension("glsl")).unwrap();
+    let mut frag_glsl = std::fs::read_to_string(frag_file.with_extension("glsl")).unwrap();
+
+    vert_glsl = annotate_vertex(vert_glsl, metadata);
+    std::fs::write(vert_file.with_extension("glsl"), vert_glsl).unwrap();
+
+    frag_glsl = annotate_fragment(frag_glsl, metadata);
+    std::fs::write(frag_file.with_extension("glsl"), frag_glsl).unwrap();
+}
+
+fn extract_shader(shader_tools: &str, binary_file: &Path) -> std::process::Child {
+    std::process::Command::new(shader_tools)
+        .args([binary_file, &binary_file.with_extension("glsl")])
+        .spawn()
+        .unwrap()
 }
 
 fn vertex_fragment_binaries<'a>(spch: &'a Spch, slct: &Slct) -> Vec<(&'a [u8], &'a [u8])> {
