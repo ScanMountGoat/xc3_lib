f5c70e64c5894da3310f9b05d2d722c95fc89a8d
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-01-05 12:59:34 -0600

    improve asmb v1 writing

--- xc3_lib/src/bc/asmb.rs
+++ xc3_lib/src/bc/asmb.rs
@@ -2,7 +2,7 @@ use crate::{parse_ptr64, parse_string_opt_ptr64, parse_string_ptr64};
 use binrw::BinRead;
 use xc3_write::{Xc3Write, Xc3WriteOffsets};
 
-use super::{BcList, BcOffset};
+use super::{BcList, BcOffset, StringOffset, StringSection};
 
 #[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
 #[br(magic(b"ASMB"))]
@@ -20,14 +20,13 @@ pub enum AsmbInner {
     V2(AsmbInnerV2),
 }
 
-#[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
+#[derive(Debug, BinRead, Xc3Write)]
 pub struct AsmbInnerV1 {
     pub unk1: u32,
     pub unk2: i32,
-    // TODO: types?
-    pub unk3: BcList<u8>,
+    pub folders: BcList<StringOffset>,
     pub unk4: BcList<BcOffset<StateV1>>,
-    pub unk5: BcList<u8>,
+    pub unk5: BcList<VarParamV1>,
     pub unk6: BcList<AnimationV1>,
 
     // TODO: This doesn't always match the chr name?
@@ -67,9 +66,20 @@ pub struct AnimationV1 {
 }
 
 #[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
-pub struct AnimationUnk1V1 {
+pub struct VarParamV1 {
     pub unk1: u32,
     pub unk2: i32,
+
+    #[br(parse_with = parse_string_ptr64)]
+    #[xc3(offset(u64))]
+    pub name: String,
+}
+
+#[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
+pub struct AnimationUnk1V1 {
+    pub unk1: u16,
+    pub unk2: u16,
+    pub unk3: i32,
 }
 
 // TODO: more fields?
@@ -111,7 +121,7 @@ pub struct StateKeyValueV1 {
 
 #[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
 pub struct AsmbInnerV2 {
-    pub unk1: BcList<u64>,
+    pub folders: BcList<StringOffset>,
     pub unk2: BcList<FsmGroupV2>,
     pub unk3: u64,        // 0?
     pub unk4: BcList<u8>, // TODO: type?
@@ -187,4 +197,79 @@ pub struct StateTransitionV2 {
     pub unk11: u32,
     pub unk12: u32,
     pub unk13: i32,
+}
+
+// TODO: Is there a cleaner way to defer and sort strings?
+impl<'a> Xc3WriteOffsets for AsmbInnerV1Offsets<'a> {
+    fn write_offsets<W: std::io::prelude::Write + std::io::prelude::Seek>(
+        &self,
+        writer: &mut W,
+        base_offset: u64,
+        data_ptr: &mut u64,
+    ) -> xc3_write::Xc3Result<()> {
+        let mut string_section = StringSection::default();
+
+        // Different order than field order.
+        let folders = self
+            .folders
+            .elements
+            .write_offset(writer, base_offset, data_ptr)?;
+        for f in folders.0 {
+            string_section.insert_offset(&f.name);
+        }
+
+        let unk5 = self
+            .unk5
+            .elements
+            .write_offset(writer, base_offset, data_ptr)?;
+        for u in unk5.0 {
+            string_section.insert_offset(&u.name);
+        }
+
+        let unk6 = self
+            .unk6
+            .elements
+            .write_offset(writer, base_offset, data_ptr)?;
+        for u in unk6.0 {
+            string_section.insert_offset(&u.file_name);
+        }
+
+        // TODO: find a better way to handle nested data.
+        let unk4 = self
+            .unk4
+            .elements
+            .write_offset(writer, base_offset, data_ptr)?;
+        for u in unk4.0 {
+            let u = u.value.write_offset(writer, base_offset, data_ptr)?;
+            string_section.insert_offset(&u.name);
+
+            let children = u
+                .children
+                .elements
+                .write_offset(writer, base_offset, data_ptr)?;
+            for c in children.0 {
+                let c = c.value.write_offset(writer, base_offset, data_ptr)?;
+                string_section.insert_offset(&c.name);
+            }
+        }
+
+        let unk8 = self
+            .unk8
+            .elements
+            .write_offset(writer, base_offset, data_ptr)?;
+        for u in unk8.0 {
+            string_section.insert_offset(&u.key);
+            string_section.insert_offset(&u.value);
+        }
+
+        // TODO: How to handle an optional string?
+        // if let Some(name) = &self.skeleton_file_name.write_offset(writer, base_offset, data_ptr)? {
+        //     string_section.insert_offset(name);
+        // }
+
+        // The names are the last item before the addresses.
+        string_section.write(writer, data_ptr, 8)?;
+
+        Ok(())
+    }
 }
