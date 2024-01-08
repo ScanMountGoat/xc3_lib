e0f30d111c4568156ab952bd1e387972e2647ecd
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-01-08 16:24:51 -0600

    improve laps writing

--- xc3_lib/src/laps.rs
+++ xc3_lib/src/laps.rs
@@ -9,9 +9,9 @@
 //! | Xenoblade Chronicles 3 |  |  |
 use crate::{parse_offset32_count32, parse_string_ptr32};
 use binrw::BinRead;
-use xc3_write::{Xc3Write, Xc3WriteOffsets};
+use xc3_write::{round_up, Xc3Write, Xc3WriteOffsets};
 
-#[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
+#[derive(Debug, BinRead, Xc3Write)]
 #[br(magic(b"LAPS"))]
 #[xc3(magic(b"LAPS"))]
 pub struct Laps {
@@ -34,7 +34,7 @@ pub struct Laps {
     pub unk5: u32,
 
     // TODO: padding?
-    pub unk: [u32; 6],
+    pub unk: [u32; 5],
 }
 
 #[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets)]
@@ -67,4 +67,31 @@ pub struct Unk3 {
     pub unk7: [f32; 5],
     pub unk8: u32,
     pub unk9: u32,
+}
+
+impl<'a> Xc3WriteOffsets for LapsOffsets<'a> {
+    fn write_offsets<W: std::io::prelude::Write + std::io::prelude::Seek>(
+        &self,
+        writer: &mut W,
+        base_offset: u64,
+        data_ptr: &mut u64,
+    ) -> xc3_write::Xc3Result<()> {
+        // Strings at the end of the file.
+        let unk2 = self.unk2.write_offset(writer, base_offset, data_ptr)?;
+        let unk3 = self.unk3.write_offset(writer, base_offset, data_ptr)?;
+
+        for u in unk2.0 {
+            u.write_offsets(writer, base_offset, data_ptr)?;
+        }
+
+        for u in unk3.0 {
+            u.write_offsets(writer, base_offset, data_ptr)?;
+        }
+
+        // Align the file size to 16.
+        let padding = round_up(*data_ptr, 16) - *data_ptr;
+        vec![0u8; padding as usize].xc3_write(writer, data_ptr)?;
+
+        Ok(())
+    }
 }
