ecb9f6524dfa331aaa65b5b9d4f315911f1a4425
RoccoDev <hey@rocco.dev>
2024-06-20 03:11:24 +0200

    fnt: document unknown fields

--- xc3_lib/src/fnt.rs
+++ xc3_lib/src/fnt.rs
@@ -24,7 +24,7 @@ pub struct Fnt {
 
     #[br(parse_with = parse_ptr32)]
     #[xc3(offset(u32))]
-    pub font: Font,
+    pub font: XcxFont,
 
     // No problem with sub-cursors here, XCX expects the MTXT footer to end the file
     // (i.e. mtxt size = file_size - textures_offset)
@@ -35,46 +35,67 @@ pub struct Fnt {
 
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 #[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets, PartialEq, Clone)]
-pub struct Font {
+pub struct XcxFont {
     pub grid_width: u32,
     pub grid_height: u32,
     glyph_count: u32,
 
+    /// Some sort of threshold, setting it too high makes some of the text not render.  
+    /// Setting it to zero is a safe bet.
     pub unk_1: u32,
+    /// Final width property for glyph rendering.
+    ///
+    /// Glyph indexes use the regular grid dimensions ([`grid_width`] x [`grid_height`]). To render
+    /// the glyph, the game then clips/extends the sprite to this width.
+    ///
+    /// Unlike [`Laft`], glyph-specific x/width does not shift the sprite box (potentially overlapping
+    /// other glyphs), instead it just controls how much whitespace is rendered before and after the glyph.
+    ///
+    /// [`Laft`]: crate::laft::Laft
+    pub subgrid_width: u32,
+    /// No visual differences when changed
     pub unk_2: u32,
-    pub unk_3: u32,
 
     pub font_height: u32,
     pub glyphs_per_row: u32,
     pub num_rows: u32,
 
     #[br(count = glyph_count)]
-    glyphs: Vec<Glyph>,
+    glyphs: Vec<XcxGlyph>,
 }
 
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 #[derive(Debug, BinRead, Xc3Write, Xc3WriteOffsets, PartialEq, Clone, Copy)]
-pub struct Glyph {
+pub struct XcxGlyph {
+    /// UTF-16 code point, acting as the primary key for this glyph.
+    ///
+    /// Both [`XcxFont::glyphs`] and the grid in the texture sheet must be ordered by this key.
     pub code_utf16: u16,
+    /// Shift-JIS code point, allows duplicates (e.g. unsupported characters share it with a similar
+    /// character) and needs not be a sort key.
     pub code_shift_jis: u16,
+
+    /// X offset relative to the next glyph (higher values result in the glyph getting shifted
+    /// farther to the left)
     pub x_offset: u8,
+    /// Additional whitespace after the glyph
     pub width: u8,
 }
 
-impl Font {
+impl XcxFont {
     /// Returns the registered glyphs, in UTF-16 code point order
-    pub fn glyphs(&self) -> &[Glyph] {
+    pub fn glyphs(&self) -> &[XcxGlyph] {
         &self.glyphs
     }
 
-    pub fn get_glyph_by_utf16(&self, code_utf16: u16) -> Option<&Glyph> {
+    pub fn get_glyph_by_utf16(&self, code_utf16: u16) -> Option<&XcxGlyph> {
         self.glyphs
             .binary_search_by_key(&code_utf16, |g| g.code_utf16)
             .ok()
             .map(|i| &self.glyphs[i])
     }
 
-    pub fn get_glyph_by_shift_jis(&self, code_shift_jis: u16) -> Option<&Glyph> {
+    pub fn get_glyph_by_shift_jis(&self, code_shift_jis: u16) -> Option<&XcxGlyph> {
         self.glyphs
             .iter()
             .find(|g| g.code_shift_jis == code_shift_jis)
@@ -84,7 +105,7 @@ impl Font {
     ///
     /// Duplicate Shift-JIS code points are allowed, while duplicate UTF-16 codes are not. The
     /// function panics if a glyph with the same UTF-16 code point is already registered.
-    pub fn register_glyph(&mut self, glyph: Glyph) {
+    pub fn register_glyph(&mut self, glyph: XcxGlyph) {
         let idx = self
             .glyphs
             .binary_search_by_key(&glyph.code_utf16, |g| g.code_utf16)
