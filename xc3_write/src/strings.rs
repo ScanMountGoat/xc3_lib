82c83f5a9c4955658d4aefb46fcefbf8fabc3eb3
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2025-02-12 10:24:05 -0600

    reduce repetition in string section logic

--- xc3_write/src/strings.rs
+++ xc3_write/src/strings.rs
@@ -9,7 +9,7 @@ use crate::{Endian, Offset, Xc3Result, Xc3Write};
 
 // TODO: support 32 and 64 bit offsets.
 
-/// Shared offsets to unique strings in alphabetical order.
+/// Offsets to unique strings in alphabetical order.
 #[derive(Debug, Clone, Default)]
 pub struct StringSectionUniqueSorted {
     // Unique strings are stored in alphabetical order.
@@ -17,6 +17,7 @@ pub struct StringSectionUniqueSorted {
 }
 
 impl StringSectionUniqueSorted {
+    /// Insert a 64-bit offset to update later.
     pub fn insert_offset(&mut self, offset: &Offset<'_, u64, String>) {
         self.name_to_offsets
             .entry(offset.data.clone())
@@ -24,6 +25,7 @@ impl StringSectionUniqueSorted {
             .push(offset.position);
     }
 
+    /// Write the strings at `data_ptr` and update all stored offsets.
     pub fn write<W: Write + Seek>(
         &self,
         writer: &mut W,
@@ -31,41 +33,25 @@ impl StringSectionUniqueSorted {
         alignment: u64,
         endian: Endian,
     ) -> Xc3Result<()> {
-        // Write the string data.
-        // TODO: Cleaner way to handle alignment?
-        let mut name_to_position = BTreeMap::new();
-        writer.seek(SeekFrom::Start(*data_ptr))?;
-        align(writer, *data_ptr, alignment, 0xff)?;
+        let name_to_position =
+            write_strings(self.name_to_offsets.keys(), writer, data_ptr, alignment)?;
 
-        for name in self.name_to_offsets.keys() {
-            let offset = writer.stream_position()?;
-            writer.write_all(name.as_bytes())?;
-            writer.write_all(&[0u8])?;
-            name_to_position.insert(name, offset);
-        }
-        *data_ptr = (*data_ptr).max(writer.stream_position()?);
-
-        // Update offsets.
         for (name, offsets) in &self.name_to_offsets {
-            for offset in offsets {
-                let position = name_to_position[name];
-                // Assume all string pointers are 8 bytes.
-                writer.seek(SeekFrom::Start(*offset))?;
-                position.xc3_write(writer, endian)?;
-            }
+            update_offsets(&name_to_position, writer, 0, endian, name, offsets)?;
         }
 
         Ok(())
     }
 }
 
-/// Shared offsets to unique strings in insertion order.
+/// Offsets to unique strings in insertion order.
 #[derive(Default)]
 pub struct StringSectionUnique {
     name_to_offsets: IndexMap<String, Vec<u64>>,
 }
 
 impl StringSectionUnique {
+    /// Insert a 32-bit offset to update later.
     pub fn insert_offset(&mut self, offset: &Offset<'_, u32, String>) {
         self.name_to_offsets
             .entry(offset.data.clone())
@@ -73,6 +59,7 @@ impl StringSectionUnique {
             .push(offset.position);
     }
 
+    /// Write the strings at `data_ptr` and update all stored offsets.
     pub fn write<W: Write + Seek>(
         &self,
         writer: &mut W,
@@ -81,33 +68,61 @@ impl StringSectionUnique {
         alignment: u64,
         endian: Endian,
     ) -> Xc3Result<()> {
-        // Write the string data.
-        // TODO: Cleaner way to handle alignment?
-        let mut name_to_position = IndexMap::new();
-        writer.seek(SeekFrom::Start(*data_ptr))?;
-        align(writer, *data_ptr, alignment, 0xff)?;
+        let name_to_position =
+            write_strings(self.name_to_offsets.keys(), writer, data_ptr, alignment)?;
 
-        for name in self.name_to_offsets.keys() {
-            let offset = writer.stream_position()?;
-            writer.write_all(name.as_bytes())?;
-            writer.write_all(&[0u8])?;
-            name_to_position.insert(name, offset);
-        }
-        *data_ptr = (*data_ptr).max(writer.stream_position()?);
-
-        // Update offsets.
         for (name, offsets) in &self.name_to_offsets {
-            for offset in offsets {
-                let position = name_to_position[name];
-                // Assume all string pointers are 4 bytes.
-                writer.seek(SeekFrom::Start(*offset))?;
-                let final_offset = position - base_offset;
-                (final_offset as u32).xc3_write(writer, endian)?;
-            }
+            update_offsets(
+                &name_to_position,
+                writer,
+                base_offset,
+                endian,
+                name,
+                offsets,
+            )?;
         }
 
         Ok(())
     }
+}
+
+fn update_offsets<W: Write + Seek>(
+    name_to_position: &BTreeMap<&String, u64>,
+    writer: &mut W,
+    base_offset: u64,
+    endian: Endian,
+    name: &String,
+    offsets: &[u64],
+) -> Result<(), std::io::Error> {
+    Ok(for offset in offsets {
+        let position = name_to_position[name];
+        // Assume all string pointers are 4 bytes.
+        writer.seek(SeekFrom::Start(*offset))?;
+        let final_offset = position - base_offset;
+        (final_offset as u32).xc3_write(writer, endian)?;
+    })
+}
+
+fn write_strings<'a, W: Write + Seek>(
+    names: impl Iterator<Item = &'a String>,
+    writer: &mut W,
+    data_ptr: &mut u64,
+    alignment: u64,
+) -> Xc3Result<BTreeMap<&'a String, u64>> {
+    // TODO: Cleaner way to handle alignment?
+    let mut name_to_position = BTreeMap::new();
+    writer.seek(SeekFrom::Start(*data_ptr))?;
+    align(writer, *data_ptr, alignment, 0xff)?;
+
+    for name in names {
+        let offset = writer.stream_position()?;
+        writer.write_all(name.as_bytes())?;
+        writer.write_all(&[0u8])?;
+        name_to_position.insert(name, offset);
+    }
+    *data_ptr = (*data_ptr).max(writer.stream_position()?);
+
+    Ok(name_to_position)
 }
 
 fn align<W: Write>(writer: &mut W, size: u64, align: u64, pad: u8) -> std::io::Result<()> {
--- xc3_write/tests/string_section_unique_sorted.rs
+++ xc3_write/tests/string_section_unique_sorted.rs
@@ -7,16 +7,17 @@ use xc3_write::{assert_hex_eq, strings::StringSectionUniqueSorted, Endian, Offse
 fn write_string_section_unique_sorted() {
     let mut section = StringSectionUniqueSorted::default();
 
-    section.insert_offset(&Offset::new(0, &String::from("def"), None, 0u8));
-    section.insert_offset(&Offset::new(8, &String::from("abc"), None, 0u8));
+    section.insert_offset(&Offset::new(0, &"def".to_string(), None, 0u8));
+    section.insert_offset(&Offset::new(8, &"abc".to_string(), None, 0u8));
+    section.insert_offset(&Offset::new(16, &"def".to_string(), None, 0u8));
 
     let mut writer = Cursor::new(Vec::new());
     section
-        .write(&mut writer, &mut 17, 4, Endian::Little)
+        .write(&mut writer, &mut 25, 4, Endian::Little)
         .unwrap();
 
     assert_hex_eq!(
-        hex!(18000000 00000000 14000000 00000000 00ffffff 61626300 64656600),
+        hex!(20000000 00000000 1c000000 00000000 20000000 00000000 00ffffff 61626300 64656600),
         writer.into_inner()
     );
 }
