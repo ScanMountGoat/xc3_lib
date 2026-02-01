fe5b1982419c1b5b021bd92fbc9a0f1a8a841b1f
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2026-02-01 11:47:14 -0600

    simplify batch render image saving

--- xc3_wgpu_batch/src/main.rs
+++ xc3_wgpu_batch/src/main.rs
@@ -1,12 +1,18 @@
-use std::{path::Path, sync::Mutex};
+use std::{
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
 
 use clap::{Parser, ValueEnum};
 use futures::executor::block_on;
 use glam::{Mat4, Vec3, vec3};
-use image::ImageBuffer;
 use rayon::prelude::*;
+use tracing::trace_span;
 use xc3_model::{load_animations, shader_database::ShaderDatabase};
 use xc3_wgpu::{CameraData, MonolibShaderTextures, Renderer};
+
+#[cfg(feature = "tracing")]
+use tracing_subscriber::prelude::*;
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 1024;
@@ -73,6 +79,12 @@ fn main() {
         .with_level(log::LevelFilter::Error)
         .init()
         .unwrap();
+
+    #[cfg(feature = "tracing")]
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::registry().with(tracing_tracy::TracyLayer::default()),
+    )
+    .unwrap();
 
     let backends = match &cli.backend {
         Some(backend) => match backend.to_lowercase().as_str() {
@@ -156,42 +168,40 @@ fn main() {
 
     // TODO: Output which model fails if there is a panic?
     let ext = cli.extension.ext();
-    globwalk::GlobWalkerBuilder::from_patterns(&cli.root_folder, &[format!("*.{ext}")])
-        .build()
-        .unwrap()
-        .par_bridge()
-        .for_each(|entry| {
-            // Create a unique buffer to avoid mapping a buffer from multiple threads.
-            let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-                size: WIDTH as u64 * HEIGHT as u64 * 4,
-                usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
-                label: None,
-                mapped_at_creation: false,
-            });
+    let paths = collect_paths(&cli.root_folder, &[format!("*.{ext}")]);
+    paths.par_iter().for_each(|path| {
+        // Create a unique buffer to avoid mapping a buffer from multiple threads.
+        let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            size: WIDTH as u64 * HEIGHT as u64 * 4,
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+            label: None,
+            mapped_at_creation: false,
+        });
 
-            let path = entry.as_ref().unwrap().path();
-            let model_path = path.to_string_lossy().to_string();
+        let model_path = path.to_string_lossy().to_string();
 
-            let groups = load_groups(
-                &device,
-                &queue,
-                &model_path,
-                cli.extension,
-                monolib_shader,
-                database.as_ref(),
-            );
+        let groups = load_groups(
+            &device,
+            &queue,
+            &model_path,
+            cli.extension,
+            monolib_shader,
+            database.as_ref(),
+        );
 
-            match groups {
-                Ok(groups) => {
-                    if cli.anim {
-                        find_and_apply_idle_anim(&queue, path, &groups);
-                    }
+        match groups {
+            Ok(groups) => {
+                if cli.anim {
+                    find_and_apply_idle_anim(&queue, path, &groups);
+                }
 
-                    for (group_index, group) in groups.iter().enumerate() {
-                        let groups = &groups[group_index..group_index + 1];
+                for (group_index, group) in groups.iter().enumerate() {
+                    let groups = &groups[group_index..group_index + 1];
 
-                        for (models_index, models) in group.models.iter().enumerate() {
-                            for (model_index, model) in models.models.iter().enumerate() {
+                    for (models_index, models) in group.models.iter().enumerate() {
+                        for (model_index, model) in models.models.iter().enumerate() {
+                            let span = trace_span!("render_model");
+                            span.in_scope(|| {
                                 let mut encoder = device.create_command_encoder(
                                     &wgpu::CommandEncoderDescriptor {
                                         label: Some("Render Encoder"),
@@ -233,9 +243,6 @@ fn main() {
                                     },
                                     size,
                                 );
-                                queue.submit([encoder.finish()]);
-
-                                drop(renderer);
 
                                 let output_path = if cli.extension == FileExtension::Wismhd {
                                     path.with_extension(format!(
@@ -244,14 +251,27 @@ fn main() {
                                 } else {
                                     path.with_extension("png")
                                 };
-                                save_screenshot(&device, &output_buffer, output_path);
-                            }
+
+                                let buffer = output_buffer.clone();
+                                encoder.map_buffer_on_submit(
+                                    &output_buffer,
+                                    wgpu::MapMode::Read,
+                                    0..,
+                                    move |result| {
+                                        if let Ok(()) = result {
+                                            save_screenshot(&buffer, output_path);
+                                        }
+                                    },
+                                );
+                                queue.submit([encoder.finish()]);
+                            });
                         }
                     }
                 }
-                Err(e) => println!("Error loading {model_path:?}: {e:?}"),
             }
-        });
+            Err(e) => println!("Error loading {model_path:?}: {e:?}"),
+        }
+    });
 
     println!("Finished in {:?}", start.elapsed());
 }
@@ -281,6 +301,7 @@ fn find_and_apply_idle_anim(queue: &wgpu::Queue, path: &Path, groups: &[xc3_wgpu
         .find(|p| apply_anim(queue, groups, p, &model_name));
 }
 
+#[tracing::instrument(skip_all)]
 fn load_groups(
     device: &wgpu::Device,
     queue: &wgpu::Queue,
@@ -365,33 +386,17 @@ fn frame_bounds(queue: &wgpu::Queue, renderer: &mut Renderer, min_xyz: Vec3, max
     renderer.update_camera(queue, &camera_data);
 }
 
-fn save_screenshot(
-    device: &wgpu::Device,
-    output_buffer: &wgpu::Buffer,
-    output_path: std::path::PathBuf,
-) {
+#[tracing::instrument(skip_all)]
+fn save_screenshot(output_buffer: &wgpu::Buffer, output_path: PathBuf) {
     // Save the output texture.
-    // Adapted from WGPU Example https://github.com/gfx-rs/wgpu/tree/master/wgpu/examples/capture
-    {
-        // TODO: Find ways to optimize this?
-        let buffer_slice = output_buffer.slice(..);
-
-        // TODO: Reuse the channel?
-        let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
-        buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
-            tx.send(result).unwrap();
-        });
-        device.poll(wgpu::PollType::wait_indefinitely()).unwrap();
-        block_on(rx.receive()).unwrap().unwrap();
-
-        let data = buffer_slice.get_mapped_range();
-        let mut buffer =
-            ImageBuffer::<image::Rgba<u8>, _>::from_raw(WIDTH, HEIGHT, data.to_owned()).unwrap();
-        // Force opaque.
-        buffer.pixels_mut().for_each(|p| p[3] = 255u8);
+    let buffer_slice = output_buffer.slice(..);
+    let data = buffer_slice.get_mapped_range();
+    rayon::spawn(move || {
+        let mut buffer = image::RgbaImage::from_raw(WIDTH, HEIGHT, data.to_owned()).unwrap();
+        // Force opaque to match sm4sh_viewer.
+        buffer.pixels_mut().for_each(|rgba| rgba[3] = 255u8);
         buffer.save(output_path).unwrap();
-    }
-    output_buffer.unmap();
+    });
 }
 
 // TODO: Move to xc3_wgpu?
@@ -416,4 +421,13 @@ fn calculate_camera_data(width: u32, height: u32, translation: Vec3, rotation: V
         width,
         height,
     }
+}
+
+fn collect_paths(root: &str, patterns: &[String]) -> Vec<PathBuf> {
+    // Collect paths ahead of time since par_iter is faster than par_bridge.
+    globwalk::GlobWalkerBuilder::from_patterns(root, patterns)
+        .build()
+        .unwrap()
+        .filter_map(|e| Some(e.ok()?.path().to_owned()))
+        .collect()
 }
